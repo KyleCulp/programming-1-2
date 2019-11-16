37a22c4a6cd9017efb4d8aaea1300fde224269ec
Kyle <me@kyleculp.com>
2019-11-16 14:58:06 -0600

    Create insuranceCost.cpp

---
+++ Payroll Program/insuranceCost.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <iostream>
+#include "insuranceCost.h"
+using namespace std;
+
+// Author: Kyle Culp
+// File: insuranceCost.cpp
+
+// Would use a switch here, but messing with the ints seemed 
+// counter productive compared to 3 if statements
+double insuranceCost(char inscode) {
+	if(inscode == 'N') {
+		return 0;
+	}
+	if(inscode == 'S') {
+		return 9.50;
+	}
+	if(inscode == 'F') {
+		return 24.75;
+	}
+	// default return 0 
+	return 0;
+}
